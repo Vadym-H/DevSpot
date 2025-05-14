56c59e7e3ea361aa1a1151e0040da71f8884308d
Vadim Huschin <vadymhuschin@gmail.com>
2025-05-14 13:28:36 +0200

    Add JobPostingController logic and UI

--- DevSpot/Controllers/JobPostingsController.cs
+++ DevSpot/Controllers/JobPostingsController.cs
@@ -1,10 +1,15 @@
-﻿using DevSpot.Models;
+﻿using DevSpot.Const;
+using DevSpot.Models;
 using DevSpot.Repositories;
+using DevSpot.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
 
 namespace DevSpot.Controllers
 {
+    [Authorize]
     public class JobPostingsController : Controller
     {
         private readonly IRepository<JobPosting> _repository;
@@ -16,19 +21,66 @@ namespace DevSpot.Controllers
             _repository = repository;
             _userManager = userManager;
         }
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
-            var jobPostings = await _repository.GetAllAsync();  
-            return View();
+            var allJobPostings = await _repository.GetAllAsync();
+
+            if (User.IsInRole(Roles.Employer))
+            {
+                var UserId = _userManager.GetUserId(User);
+                var filteredJobPostings = allJobPostings.Where(jp => jp.UserId == UserId);
+
+                return View(filteredJobPostings);
+            }
+            return View(allJobPostings);
         }
+        [Authorize(Roles = Roles.Admin + "," + Roles.Employer)]
+
         public IActionResult Create()
         {
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> Create(JobPosting jobPosting)
+        [Authorize(Roles = "Admin,Employer")]
+        public async Task<IActionResult> Create(JobPostingViewModel jobPostingVm)
         {
-            return RedirectToAction(nameof(Index));
+            if (ModelState.IsValid)
+            {
+                var jobPosting = new JobPosting()
+                {
+                    Title = jobPostingVm.Title,
+                    Description = jobPostingVm.Description,
+                    Company = jobPostingVm.Company,
+                    Lacation = jobPostingVm.Lacation,
+                    UserId = _userManager.GetUserId(User)
+                };
+                await _repository.AddAsync(jobPosting);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(jobPostingVm);
+        }
+        [HttpDelete]
+        [Authorize(Roles = "Admin,Employer")]
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            var jobPosting = await _repository.GetByIdAsync(id);
+
+            if (jobPosting == null)
+            {
+                return NotFound();
+            }
+             
+            var userId = _userManager.GetUserId(User);
+
+            if(User.IsInRole(Roles.Admin) == false && jobPosting.UserId != userId)
+            {
+                return Forbid();
+            }
+
+            await _repository.DeleteAsync(id);
+            return Ok();
         }
     }
 }
---
+++ DevSpot/Migrations/20250513135223_RenameCompamyToCompany.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevSpot.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameCompamyToCompany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+            name: "Compamy",
+            table: "JobPostings",
+            newName: "Company");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+            name: "Company",
+            table: "JobPostings",
+            newName: "Compamy");
+        }
+    }
+}
